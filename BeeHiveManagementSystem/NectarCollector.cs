4aa5be70f83ab74553cd59228e91f928a35181ed
Rasmus <Rasmus@vinperlen.dk>
2021-12-12 13:29:23 +0100

    Refactor

--- BeeHiveManagementSystem/NectarCollector.cs
+++ BeeHiveManagementSystem/NectarCollector.cs
@@ -9,7 +9,7 @@ namespace BeeHiveManagementSystem
     public class NectarCollector : Bee
     {
         private const float NECTAR_COLLECTED_PER_SHIFT = 33.25f;
-        public override float CostPerShift { get { return 1.95f; } }
+        public override float CostPerShift { get =>  1.95f; }
         public NectarCollector() : base("Nectar Collector") { }
         protected override void DoJob()
         {
